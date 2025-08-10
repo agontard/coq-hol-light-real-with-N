ba994d8d61c6c784552e21287be5568865220ba7
Antoine Gontard <antoine.gontard@free.fr>
2025-08-10 17:39:24 +0200

    fix

--- mappings.v
+++ mappings.v
@@ -887,10 +887,9 @@ Ltac solve_total_align := try full_destruct ;
    See term_align in coq-hol-light-Logic1 for an example with lists as recursive arguments,
    using the following solving tactic *)
 
-
 Ltac solve_total_align_with_lists :=
   try full_destruct ; blindrewrite ; auto ;
-  repeat (f_equal ; try now apply map_ext_Forall).
+  repeat (f_equal ; try now apply List.map_ext_Forall).
 
 Ltac use_induction r := induction r.
 Ltac total_align1 := total_align1_general use_induction solve_total_align.
